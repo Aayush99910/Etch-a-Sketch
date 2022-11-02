b3eeb0111127800d4c7f9a5f480ebf8b10c35073
Aayush99910 <108318435+Aayush99910@users.noreply.github.com>
2022-11-02 12:18:55 +0545

    Grid feature is working and added clearbtn feature as well. FINAL PRODUCT

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// grabbing all the necessary elements from the document
-const colors = document.querySelectorAll(".co");
-const eraserBtn = document.querySelector("#eraser");
-const colorBtn = document.querySelector("#color");
-const COLOR = document.querySelector("#colorpicker");
-const paintDiv = document.querySelector(".paint-pad");
-
-
 // some variables
 let defaultColor = "black"; // default color in the board
 let whiteColor = "white";   // white color
@@ -13,6 +5,8 @@ let eraserOn = false; // checks if the erase is toggled
 let newColor; // assigns newcolor to the brush
 let mouseDown = false; // checks if the mouse is down or not
 
+
+// lets the user draw after they have selected the grid
 function userDraw () {
     const grid = document.querySelectorAll(".grid");
     const gridArray = Array.from(grid); // converting NODELIST into an array
@@ -35,6 +29,54 @@ function userDraw () {
 }
 
 
+// removes the color of all the div and changes it back to white
+function removeAll() {
+    const grid = document.querySelectorAll(".grid");
+    const gridArray = Array.from(grid); // converting NODELIST into an array
+    gridArray.forEach((eachgrid) => {
+        eachgrid.style.backgroundColor = whiteColor;
+    });
+}
+
+
+// creates a grid of user's choice || if default is passed 10 X 10 grid is created
+// else user's input is created. This function also changes the style of 
+// the grid after the input. Lastly it creates the requird amount of 
+// div using a for loop.
+function createGrid (choice) {
+    let divThatShouldBeCreated;
+    let divNum;
+    const size = document.querySelector("#size");
+    const gridContainer = document.querySelector(".paint-pad");
+
+    if (choice === "default") {
+        divThatShouldBeCreated = 100;
+        divNum = 10;
+        gridContainer.innerHTML = " ";
+        gridContainer.style.gridTemplateColumns = `repeat(${divNum}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${divNum}, 1fr)`;
+                
+    }else{
+        divNum = slider.value;
+        divThatShouldBeCreated = Number(divNum) * Number(divNum);
+        gridContainer.innerHTML = " ";
+        gridContainer.style.gridTemplateColumns = `repeat(${divNum}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${divNum}, 1fr)`;
+    }
+
+    size.textContent = `${divNum} X ${divNum}`;
+    for (let i = 0; i < divThatShouldBeCreated; i++) {
+        const div = document.createElement('div');
+        div.classList.add("grid");
+        gridContainer.appendChild(div);
+    }     
+
+    userDraw();
+}
+
+
+// changes color to new color from the given color pallete 
+const colors = document.querySelectorAll(".co");
 const colorsArray = Array.from(colors); // converting NODELIST to an array
 colorsArray.forEach(function (color) {
     color.addEventListener("click", () => {
@@ -43,7 +85,9 @@ colorsArray.forEach(function (color) {
     });
 });
 
-// event listener for eraser btn
+
+// event listener for eraser btn? / toggles the eraser button
+const eraserBtn = document.querySelector("#eraser");
 eraserBtn.addEventListener('click', () => {
     const eraserText = document.querySelector("#on");
     // toggles the button
@@ -56,7 +100,10 @@ eraserBtn.addEventListener('click', () => {
     }
 });
 
-// event listener for the color btn
+
+// event listener for the color btn // when clicked on the button it
+// shows four color option and makes the text black
+const colorBtn = document.querySelector("#color");
 colorBtn.addEventListener('click', () => {
     const colorOption = document.querySelector(".color-option");
     const colorText = document.querySelector("#colo"); // makes the textcolor black when selected
@@ -70,10 +117,14 @@ colorBtn.addEventListener('click', () => {
 });
 
 
+// changes the brush's color to user's choice
+const COLOR = document.querySelector("#colorpicker");
 COLOR.addEventListener("mouseout", () => {
     newColor = COLOR.value;
 });
 
+
+const paintDiv = document.querySelector(".paint-pad");
 paintDiv.addEventListener("mousedown", () => {
     mouseDown = true;
 });
@@ -85,35 +136,23 @@ paintDiv.addEventListener("mouseup", () => {
 
 const gridBtn = document.querySelector("#grid"); 
 const slider = document.querySelector(".slider");
+const gridOption = document.querySelector(".grid-option");
+// toggles the grid button | when clicked shows the option and vice versa
 gridBtn.addEventListener("click", () => {
     const spangrid = document.querySelector("#gd");
-    if (window.getComputedStyle(slider).display === "none") {
-        slider.style.display = "flex";
+    if (window.getComputedStyle(gridOption).display === "none") {
+        gridOption.style.display = "flex";
         spangrid.style.color = defaultColor;
-    } else if (window.getComputedStyle(slider).display === "flex") {
-        slider.style.display = "none";
+    } else if (window.getComputedStyle(gridOption).display === "flex") {
+        gridOption.style.display = "none";
         spangrid.style.color = whiteColor;
     }
 });
-
 slider.addEventListener("mouseup", createGrid);
 
-function createGrid () {
-    const size = document.querySelector("#size");
-    const divNum = slider.value;
-    const divThatShouldBeCreated = Number(divNum) * Number(divNum);
-    size.textContent = `${divNum} X ${divNum}`;
-    
-    const gridContainer = document.querySelector(".paint-pad");
-    gridContainer.innerHTML = " ";
-    gridContainer.style.gridTemplateColumns = `repeat(${divNum}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${divNum}, 1fr)`;
-            
-    for (let i = 0; i < divThatShouldBeCreated; i++) {
-        const div = document.createElement('div');
-        div.classList.add("grid");
-        gridContainer.appendChild(div);
-    }     
 
-    userDraw();
-}
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click", removeAll);
+
+
+createGrid("default");
